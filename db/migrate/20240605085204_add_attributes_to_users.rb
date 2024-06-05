dd0b0dc23609e7cd1f7a2a8cf0c6997d64520e59
ThibaultL24 <thibault.lenormand24@gmail.com>
2024-06-05 10:54:28 +0200

    Devise

---
+++ db/migrate/20240605085204_add_attributes_to_users.rb
@@ -0,0 +1,8 @@
+class AddAttributesToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :age, :integer
+    add_column :users, :is_admin, :boolean, default: false 
+  end
+end
