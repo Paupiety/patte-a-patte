4278a54393995dc76cbd54036590b8bb71e1a8cc
ThibaultL24 <thibault.lenormand24@gmail.com>
2024-06-05 18:14:30 +0200

    stripe activated

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   get 'static_pages/accueil'
   root 'static_pages#accueil'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #stripe roads
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
