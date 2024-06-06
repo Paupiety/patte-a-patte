d468b883851c3289ee91d47ca46a86572b5ad1c4
ThibaultL24 <thibault.lenormand24@gmail.com>
2024-06-06 19:28:01 +0200

    Like corrigé

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_134524) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_06_06_150232) do
->>>>>>> c9a6cedc677662537955a251617331fbbb8f4757
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
