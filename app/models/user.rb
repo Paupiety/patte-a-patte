2f195b844e37b8b6b464ac17b715b0bbf4741ccd
paupiety <manonesqu@gmail.com>
2024-06-06 09:50:06 +0200

    function mailer in comment on model user because bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 class User < ApplicationRecord
-  after_create :welcome_send
+  # after_create :welcome_send
   has_one_attached :profile_picture
   has_one :cart
   has_many :orders
 
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
+  # def welcome_send
+  #   UserMailer.welcome_email(self).deliver_now
+  # end
 
-  def create_cart
-    Cart.create(user: self)
-  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
