433628285bbab21d11a60503c72c7077ab29c17a
paupiety <manonesqu@gmail.com>
2024-06-07 15:32:04 +0200

    correction syntax in model Offer for Typeoffer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :user
+  belongs_to :type_offer, class_name: 'Typeoffer'
   has_one_attached :image
   has_many :comments, dependent: :destroy
   has_many :likes
@@ -10,7 +11,6 @@ class Offer < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :type_animal, presence: true
   validates :type_offer, presence: true
-  belongs_to :type_offer
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "date_publication", "description", "id", "price", "title", "type_animal", "updated_at", "user_id"]
