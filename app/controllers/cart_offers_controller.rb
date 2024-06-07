d0a7ea122401f7c43c489ca25ef3401f7208feff
paupiety <manonesqu@gmail.com>
2024-06-07 10:21:42 +0200

    cart-front

--- app/controllers/cart_offers_controller.rb
+++ app/controllers/cart_offers_controller.rb
@@ -13,8 +13,8 @@ class CartOffersController < ApplicationController
       def destroy
         id = params[:offer_id]
         @cart = Cart.find_by(user: current_user)
-        @offer = Item.find(id)
-        @cart_offer = CartItem.find_by(cart: @cart, item: @offer)
+        @offer = Offer.find(id)
+        @cart_offer = CartOffer.find_by(cart: @cart, offer: @offer)
         @cart_offer.destroy
         redirect_back(fallback_location: root_path)
         flash[:success] = "L'article a  été supprimé de votre panier"
