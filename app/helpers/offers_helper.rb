7e0a4409094575be10dcd0dfc75ad102dc54dd92
AlexandreTedesco <alexandre.tedesco75@gmail.com>
2024-06-10 15:58:14 +0200

    formulaire de depot

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -4,6 +4,13 @@ module OffersHelper
       @offer = Offer.find(params[:id])
     end
 
+    def authenticate_offer_owner!
+      unless @offer.user_id == current_user.id
+        flash[:error] = "Vous n'êtes pas autorisé à modifier cette offre."
+        redirect_to offers_path
+      end
+    end
+
     def offer_params
       params.require(:offer).permit(:title, :description, :price, :type_animal, :date_publication, :image, :type_offer_id)
     end
