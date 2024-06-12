c7bfe5512afcc67eae19fc5907d0bd9b84b525ed
paupiety <manonesqu@gmail.com>
2024-06-12 14:49:55 +0200

    creation update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :addresses, only: [:create, :destroy]
+  resources :addresses
 
   resources :offers do
     resources :cart_offers, only: %i[create destroy]
