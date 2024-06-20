a2be501f8fc8e31fedf8509bad742ab1747fd02d
ThibaultL24 <thibault.lenormand24@gmail.com>
2024-06-20 16:35:22 +0200

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,4 @@ module ApplicationHelper
   def canonical_url
     request.original_url
   end
-
-  def offer_jsonld(offer)
-    {
-      "@context": "http://schema.org",
-      "@type": "Offer",
-      "name": offer.title,
-      "description": offer.description,
-      "price": offer.price,
-      "priceCurrency": "EUR",
-
-    }.to_json.html_safe
-  end
 end
