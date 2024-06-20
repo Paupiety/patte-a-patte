93eb2a00c5b41805d58e0ab4b5d1c854d954de2a
ThibaultL24 <thibault.lenormand24@gmail.com>
2024-06-20 16:56:22 +0200

    delete comment

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,10 +1,19 @@
 module CommentsHelper
-
   def find_offer_of_comment
     @offer = Offer.find(params[:offer_id])
   end
 
+  def set_comment
+    @comment = @offer.comments.find(params[:id])
+  end
+
   def comment_params
     params.require(:comment).permit(:title, :content)
+  end
+
+  def authorize_user!
+    unless @comment.user == current_user
+      redirect_to @offer, alert: 'Vous n\'êtes pas autorisé à supprimer ce commentaire.'
+    end
   end
 end
